c921740ba428706970e5ba4109d00565adf368bc
Kevin <kevinken@souescoteiro.org.br>
2020-05-02 18:11:01 -0300

    Desafio 04 - feito em typescript com desenvolvimento de arquitetura de sistemas

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .map(trans => trans.value)
+      .reduce((prev, curr) => prev + curr, 0);
+    const outcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .map(trans => trans.value)
+      .reduce((prev, curr) => prev + curr, 0);
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
